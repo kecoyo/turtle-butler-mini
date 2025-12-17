1ea0756a179a6811e0fadd3a56ccda8de1d2d07e
杨可可 <kecoyo@163.com>
2025-12-17 13:53:40 +0800

    nutui组件按需加载

--- babel.config.js
+++ babel.config.js
@@ -10,5 +10,34 @@ module.exports = {
       },
     ],
   ],
-  plugins: []
+  plugins: [
+    [
+      'import',
+      {
+        libraryName: '@nutui/nutui-react-taro',
+        camel2DashComponentName: false,
+        customName: (name, file) => {
+          return `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}`
+        },
+        // 自动加载 scss 样式文件
+        customStyleName: (name) =>
+          `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}/style`,
+        // 自动加载 css 样式文件
+        // customStyleName: (name) => `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}/style/css`
+
+        // JMAPP 主题
+        // 自动加载 scss 样式文件
+        // customStyleName: (name) => `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}/style-jmapp`,
+        // 自动加载 css 样式文件
+        // customStyleName: (name) => `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}/style-jmapp/css`
+
+        // jrkf 端主题
+        // 自动加载 scss 样式文件
+        // customStyleName: (name) => `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}/style-jrkf`,
+        // 自动加载 css 样式文件
+        // customStyleName: (name) => `@nutui/nutui-react-taro/dist/es/packages/${name.toLowerCase()}/style-jrkf/css`
+      },
+      'nutui-react',
+    ],
+  ]
 };
